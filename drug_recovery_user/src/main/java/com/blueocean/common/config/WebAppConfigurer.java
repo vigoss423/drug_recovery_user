962b080d2f68f5f00dbd6edbc9987f8a3644cd6c
vigoss423 <yuxinwang0303@126.com>
2017-08-17 14:27:38 +0800

    1

--- drug_recovery_user/src/main/java/com/blueocean/common/config/WebAppConfigurer.java
+++ drug_recovery_user/src/main/java/com/blueocean/common/config/WebAppConfigurer.java
@@ -13,30 +13,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
-//@EnableConfigurationProperties({ ResourceProperties.class })
 public class WebAppConfigurer  extends WebMvcConfigurerAdapter{
-	
-//    @Autowired
-//    private ResourceProperties resourceProperties = new ResourceProperties();
 
 	 @Override
 	 public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		 
-//		 Integer cachePeriod = resourceProperties.getCachePeriod();
-
 	  registry.addResourceHandler("/**").addResourceLocations("classpath:/");
-//	  registry.addResourceHandler("/*.html")
-//      .addResourceLocations("classpath:/index.html");
-//      .setCachePeriod(cachePeriod).resourceChain(true)
-//      .addResolver(new PathResourceResolver() {
-//          @Override
-//          protected Resource getResource(String resourcePath,
-//                  Resource location) throws IOException {
-//        	  System.out.println(location.toString());
-//              return location.exists() && location.isReadable() ? location
-//                      : null;
-//          }
-//      });
 	 }
 	 
 }
